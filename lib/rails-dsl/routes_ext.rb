7b45046dcaf41b05ed5140ccd7ee1f158f230804
Adam Luzsi <adam.luzsi@ppt-consulting.net>
2014-05-23 05:34:13 +0200

    update routes extension

--- lib/rails-dsl/routes_ext.rb
+++ lib/rails-dsl/routes_ext.rb
@@ -1,61 +1,85 @@
-# module Rails
-#   module DSL
-#
-#     module ActionDispatchRouteEXT
-#
-#       module BlockParser
-#
-#         def initialize &block
-#           @values ||= {}
-#           block.call
-#         end
-#
-#         def values
-#           return @values
-#         end
-#
-#
-#         @@methods ||= [:get,:post,:put,:delete]
-#         def method_missing method_name,*args
-#
-#           Rails.logger.info method_name
-#           if @@methods.include?(method_name)
-#             @values[method_name]= args[0]
-#           end
-#
-#           return nil
-#         end
-#
-#       end
-#
-#       def mapping opts={}, &block
-#         raise ArgumentError,"options must be hash!" unless opts.class <= Hash
-#
-#         { scope: [:s,:namespace,:path], resource: [:r,:class], defaults: [:d,:default] }.each do |opts_sym,aliases|
-#           aliases.each do |alias_sym|
-#             opts[opts_sym] ||= opts.delete(alias_sym) || opts.delete(alias_sym.to_s)
-#           end
-#         end
-#
-#         opts[:defaults] ||= {}
-#
-#         # raise(ArgumentError,"Invalid resource given") if [Symbol,String].select{ |klass| opts[:resource].class <= klass }.empty?
-#         raise(ArgumentError,"Invalid defaults given") unless opts[:defaults].class <= ::Hash
-#
-#         requests= Rails::DSL::ActionDispatchRouteEXT::BlockParser.new(&block)
-#
-#         generate_calls= lambda { |asd| }
-#
-#         if !opts[:scope].nil? && opts[:scope].class <= String
-#           scope opts[:scope] do
-#
-#           end
-#         end
-#
-#       end
-#
-#     end
-#   end
-# end
-#
-# ActionDispatch::Routing::Mapper.__send__ :include, Rails::DSL::ActionDispatchRouteEXT
+module Rails
+  module DSL
+
+    module ActionDispatchRouteEXT
+
+      def mount_controller *args
+
+        opts= args.select{|e|(e.class <= ::Hash)}.reduce( {}, :merge! )
+
+        # search for alt keys
+        {
+
+            scope:    [:s,:namespace,:path],
+            resource: [:r,:class],
+            defaults: [:d,:default],
+
+            get:      [:read],
+            post:     [:create],
+            put:      [:update],
+            delete:   [:delete]
+
+        }.each do |opts_sym,aliases|
+          aliases.each do |alias_sym|
+            opts[opts_sym] ||= opts.delete(alias_sym) || opts.delete(alias_sym.to_s)
+          end
+        end
+
+        # set defaults
+        opts[:defaults] ||= {}
+        opts[:resource] ||= args.select{|e|([::Class,::String,::Symbol].include?(e.class))}[0]
+
+        [:get,:post,:put,:delete,:options].each{|sym| opts[sym] ||= [] ; opts[sym]= [opts[sym]]  unless opts[sym].class <= ::Array }
+
+        # validations
+        raise(ArgumentError,"Invalid defaults given") unless opts[:defaults].class <= ::Hash
+
+        # helper lambdas
+
+        create_mapping= lambda do
+
+          class_rails_name= opts[:resource].to_s.underscore.split('_')[0]
+          klass_name      = ( opts[:resource].class == Class ? opts[:resource] : opts[:resource].to_s.concat('_controller').classify.constantize )
+          klass_name.public_instance_methods(false).each do |method_name|
+
+            method_to_use= nil
+            [:get,:post,:put,:delete,:options].each do |pre_spec_method_call_type|
+              if opts[pre_spec_method_call_type].include?(method_name)
+                method_to_use ||= pre_spec_method_call_type
+              end
+            end
+            method_to_use ||= :get
+
+            self.__send__ method_to_use,"/#{method_name}",{to: "#{class_rails_name}##{method_name}"}.merge({defaults: opts[:defaults]})
+
+          end
+
+        end
+
+        # make scope
+        if !opts[:scope].nil? && opts[:scope].class <= String
+          self.scope opts[:scope] do
+            create_mapping.call
+          end
+        else
+          create_mapping.call
+        end
+
+      end
+
+      # def mount_controller_api *args
+      #
+      #   respond_to do |format|
+      #     format.html
+      #     format.json{
+      #       render :json => {hello: 'world'}
+      #     }
+      #   end
+      #
+      # end
+
+    end
+  end
+end
+
+ActionDispatch::Routing::Mapper.__send__ :include, Rails::DSL::ActionDispatchRouteEXT
