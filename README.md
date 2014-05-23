57806927f414e920658e66cf44bd71760ad67636
Adam Luzsi <adam.luzsi@ppt-consulting.net>
2014-05-23 13:16:39 +0200

    update mount_as_api logic

--- README.md
+++ README.md
@@ -21,7 +21,9 @@ if you call rails with 'kill' / 'k' command from now on, it will kill the applic
 
 ### Routing
 
-#### mount controller
+#### Mount controller
+
+##### As pages
 
 * mount a controller public methods as routes.
 * the method name will be the path
@@ -60,5 +62,51 @@ you can use the following options (name: [aliases])
     end
 
     #> mount not private methods from PagesController
+
+```
+
+##### As API behavor
+
+in this mount options,
+* the methods return value will be sent back parsed object to the requester
+* you dont have to set up render options
+* arguments will be parsed into route :params so the othere side can cache based on url
+** the method actualy receive the passed parameter so you can use like in the example
+
+```ruby
+
+
+    #> controller
+    class PagesController < ApplicationController
+
+      #> this generate /test1/:hello.:format  path
+      def test1 hello
+        {hello: hello }
+      end
+
+      #> this generate /test2/:hello.:format  path
+      def test2
+        {hello: 'hello' }
+      end
+
+    end
+
+    #> routes.rb
+
+    HeartShoot::Application.routes.draw do
+
+      mount_controller_api :pages
+
+      # or
+      mount_as_api :pages
+      # you can still use the Class name too
+
+      # or
+      #> if you prefer the JSON output as default
+      mount_as_api :pages, default: { format: :json }
+
+    end
+
+    #> mount not private methods as apit from PagesController
 
 ```
--- lib/rails-dsl/routes_ext.rb
+++ lib/rails-dsl/routes_ext.rb
@@ -6,6 +6,28 @@ module Rails
       module Helpers
         class << self
 
+          def array_to_url_params array,method_name
+
+            return "" unless array.class <= ::Array
+
+            var=nil
+            array.each do |ary|
+
+              if ary[0].to_s == method_name.to_s
+                var = ary[1]
+                break
+              end
+
+            end
+
+            if !var.nil?
+              return "/:#{var.join('/:')}"
+            else
+              return ""
+            end
+
+          end
+
           def process_args *args
 
             opts= args.select{|e|(e.class <= ::Hash)}.reduce( {}, :merge! )
@@ -16,6 +38,7 @@ module Rails
                 scope:    [:s,:namespace,:path],
                 resource: [:r,:class],
                 defaults: [:d,:default],
+                params:   [:url_params],
 
                 get:      [:read],
                 post:     [:create],
@@ -31,15 +54,28 @@ module Rails
             # set defaults
             opts[:defaults] ||= {}
             opts[:resource] ||= args.select{|e|([::Class,::String,::Symbol].include?(e.class))}[0]
+            opts[:params] ||= args.select{|e|(e.class <= ::Array)}
 
             [:get,:post,:put,:delete,:options].each{|sym| opts[sym] ||= [] ; opts[sym]= [opts[sym]]  unless opts[sym].class <= ::Array }
+            unless opts[:params].class <= ::Array
+              raise(ArgumentError,"invalid argument for url params: #{opts[:params]}.\nmust be something like [:method_name,[:url_params]] || [:method_name,:url_params]")
+            end
+
+            opts[:params] ||= []
+            unless opts[:params].empty?
+
+              opts[:params].each{|ary| raise unless ary.size == 2 }
+              opts[:params].each{|ary| ary[1]= [ary[1]] unless ary[1].class <= ::Array }
+
+            end
+
 
             # validations
             raise(ArgumentError,"Invalid defaults given") unless opts[:defaults].class <= ::Hash
 
             opts[:short_class_name]= opts[:resource].to_s.underscore.split('_')[0]
-            opts[:class]      = ( opts[:resource].class == Class ? opts[:resource] : opts[:resource].to_s.concat('_controller').classify.constantize )
-
+            opts[:class] = ( opts[:resource].class == Class ? opts[:resource] : opts[:resource].to_s.concat('_controller').classify.constantize )
+            opts[:pim] = opts[:class].public_instance_methods(false).select{|e|(e.to_s.last != '?')}-[:default_format_xml, :default_format_json]
 
             return opts
 
@@ -57,7 +93,7 @@ module Rails
 
         create_mapping= lambda do
 
-          opts[:class].public_instance_methods(false).each do |method_name|
+          opts[:pim].each do |method_name|
 
             method_to_use= nil
             [:get,:post,:put,:delete,:options].each do |pre_spec_method_call_type|
@@ -67,7 +103,11 @@ module Rails
             end
             method_to_use ||= :get
 
-            self.__send__ method_to_use,"/#{method_name}",{to: "#{opts[:short_class_name]}##{method_name}"}.merge({defaults: opts[:defaults]})
+            puts "/#{method_name}#{Rails::DSL::ActionDispatchRouteEXT::Helpers.array_to_url_params(opts[:params],method_name)}"
+
+            self.__send__ method_to_use,
+                          "/#{method_name}#{Rails::DSL::ActionDispatchRouteEXT::Helpers.array_to_url_params(opts[:params],method_name)}",
+                          {to: "#{opts[:short_class_name]}##{method_name}"}.merge({defaults: opts[:defaults]})
 
           end
 
@@ -82,20 +122,47 @@ module Rails
           create_mapping.call
         end
 
+        return nil
+
       end
 
-      # def mount_controller_api *args
-      #   opts= Rails::DSL::ActionDispatchRouteEXT::Helpers.process_args(*args)
-      #
-      #   # respond_to do |format|
-      #   #   format.html
-      #   #   format.json{
-      #   #     render :json => {hello: 'world'}
-      #   #   }
-      #   # end
-      #
-      # end
-      # alias mount_controller_as_api mount_controller_api
+      def mount_controller_api *args
+        opts= Rails::DSL::ActionDispatchRouteEXT::Helpers.process_args(*args)
+        conv_params= []
+
+        # make last value return as value object rendered as the specific format
+        opts[:class].class_eval do
+
+          opts[:pim].each do |sym|
+
+            var= self.instance_method(sym)
+            parameters= []
+
+            unless var.parameters.select{|ary|(ary[0] ==:req)}.empty?
+              parameters += var.parameters.select{|ary|(ary[0] ==:req)}.map{|ary| ary[1] }
+              conv_params.push [ sym, parameters ]
+            end
+
+            define_method(sym) do
+              value= var.bind(self).call(*parameters.map{ |param_key| params[param_key] })
+              respond_to do |format|
+                format.html
+                format.json { render json:  value }
+                format.xml  { render xml:   value }
+              end
+            end
+
+          end
+
+        end
+
+        # mount controller methods
+        mount_controller *args,*conv_params
+
+        return nil
+
+      end
+      alias mount_as_api mount_controller_api
 
     end
   end
