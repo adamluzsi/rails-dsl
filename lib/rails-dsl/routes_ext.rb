6e20aeea6da6b7bc3243e1f06a7dd9d8d1321b54
Adam Luzsi <adam.luzsi@ppt-consulting.net>
2014-07-09 06:57:01 +0200

    clean out route ext

--- lib/rails-dsl/routes_ext.rb
+++ lib/rails-dsl/routes_ext.rb
@@ -5,6 +5,91 @@ module Rails
       module Helpers
         class << self
 
+          def process_opts opts={}
+
+            opts = {class: opts} unless opts.class <= ::Hash
+
+            # search for alt keys
+            {
+
+                urls:     [:url,:path,{}],
+                defaults: [:default,{}],
+                ne:       [:exception,:exceptions,:ex,[]],
+
+                get:      [:read],
+                post:     [:create],
+                put:      [:update],
+                delete:   [:delete],
+
+                class:    [:klass,:resource,:resources,:controller]
+
+            }.each do |opts_sym,aliases|
+              aliases.each do |alias_sym|
+                opts[opts_sym] ||= alias_sym.class == ::Symbol ? opts.delete(alias_sym) || opts.delete(alias_sym.to_s) : alias_sym
+              end
+              if aliases.last.class != ::Symbol
+                raise(ArgumentError,"Invalid object given for #{opts_sym}, should be: #{aliases.last.class}") unless opts[opts_sym].class <= aliases.last.class
+              end
+            end
+
+            # set defaults
+            opts[:ne].map!{|method_name_to_sym| method_name_to_sym.to_s.to_sym }
+            [:get,:post,:put,:delete,:options].each{|sym| opts[sym] ||= [] ; opts[sym]= [opts[sym]]  unless opts[sym].class <= ::Array }
+
+            opts[:short_class_name]= opts[:class].to_s.underscore.split('_')[0]
+            opts[:class] = if  opts[:class].class == Class
+                             opts[:class]
+
+                           else
+
+                             if opts[:class].to_s.include?('_controller')
+                               opts[:class].to_s.classify.constantize
+
+                             elsif opts[:class].to_s.include?('Controller')
+                               opts[:class].to_s.constantize
+
+                             else
+                               begin
+                                 opts[:class].to_s.concat('_controller').classify.constantize
+                               rescue NameError
+                                 if opts[:class].to_s == opts[:class].to_s.downcase
+                                   opts[:class].to_s.classify.constantize
+
+                                 else
+                                   opts[:class].to_s.constantize
+
+                                 end
+                               end
+
+                             end
+                           end
+
+            opts[:pim] = opts[:class].public_instance_methods(false).select{|e| !(%W[ ? ! _ ].include?(e.to_s.last)) } - opts[:ne]
+
+            # make setup able method configs
+            opts[:pim].each do |sym|
+
+              sym_str= sym.to_s
+              {
+                  get:    [/_get$/,/^get_/],
+                  post:   [/_post$/,/^post_/],
+                  put:    [/_put$/,/^put_/],
+                  delete: [/_delete$/,/^delete_/]
+              }.each do |type,regular_expressions|
+                regular_expressions.each do |regex|
+                  if sym_str =~ regex
+                    opts[type].push(sym)
+                    opts[:urls][sym] ||= "/#{sym_str.gsub(regex,"")}"
+                  end
+                end
+              end
+
+            end
+
+            return opts
+
+          end
+
           def array_to_url_params array,method_name
 
             return "" unless array.class <= ::Array
@@ -27,135 +112,47 @@ module Rails
 
           end
 
-          def process_args *args
-
-            opts= args.select{|e|(e.class <= ::Hash)}.reduce( {}, :merge! )
-
-            # search for alt keys
-            {
-
-                urls:     [:p,:paths,:url,:u],
-                scope:    [:s],
-                resource: [:r,:class],
-                defaults: [:d,:default],
-                params:   [:url_params],
-                ne:       [:exception,:exceptions,:ex],
-
-                get:      [:read],
-                post:     [:create],
-                put:      [:update],
-                delete:   [:delete]
-
-            }.each do |opts_sym,aliases|
-              aliases.each do |alias_sym|
-                opts[opts_sym] ||= opts.delete(alias_sym) || opts.delete(alias_sym.to_s)
-              end
-            end
-
-            # set defaults
-            opts[:defaults] ||= {}
-            opts[:resource] ||= args.select{|e|([::Class,::String,::Symbol].include?(e.class))}[0]
-
-            opts[:params]   ||= args.select{|e|(e.class <= ::Array)}
-            opts[:params]   ||= []
-
-            opts[:urls]     ||= {}
-            opts[:ne]       ||= []
-
-            opts[:ne].map!{|method_name_to_sym| method_name_to_sym.to_s.to_sym }
-
-            [:get,:post,:put,:delete,:options].each{|sym| opts[sym] ||= [] ; opts[sym]= [opts[sym]]  unless opts[sym].class <= ::Array }
-
-            unless opts[:params].class <= ::Array
-              raise(ArgumentError,"invalid argument for url params: #{opts[:params]}.\nmust be something like [:method_name,[:url_params]] || [:method_name,:url_params]")
-            end
-
-            unless opts[:urls].class <= ::Hash
-              raise(ArgumentError,"invalid argument for urls group: #{opts[:urls]}.\nmust be something like {method_name: '/path'}")
-            end
-
-            unless opts[:params].empty?
-
-              opts[:params].each{|ary| raise unless ary.size == 2 }
-              opts[:params].each{|ary| ary[1]= [ary[1]] unless ary[1].class <= ::Array }
-
-            end
-
-            # validations
-            raise(ArgumentError,"Invalid defaults given") unless opts[:defaults].class <= ::Hash
-
-            opts[:short_class_name]= opts[:resource].to_s.underscore.split('_')[0]
-            opts[:class] = ( opts[:resource].class == Class ? opts[:resource] : opts[:resource].to_s.concat('_controller').classify.constantize )
-            opts[:pim] = opts[:class].public_instance_methods(false).select{|e|(e.to_s.last != '?')} - opts[:ne]
-
-            # make setup able method configs
-            opts[:pim].each do |sym|
-
-              sym_str= sym.to_s
-              {
-                  get:    /_get$/,
-                  post:   /_post$/,
-                  put:    /_put$/,
-                  delete: /_delete$/
-              }.each do |type,regex|
-
-                if sym_str =~ regex
-                  opts[type].push(sym)
-                  opts[:urls][sym] ||= "/#{sym_str.gsub(regex,"")}"
-                end
-
-              end
-
-            end
-
-            return opts
-
-          end
-
         end
       end
 
-      def mount_controller *args
+      def mount_by(opts={})
+        opts = Helpers.process_opts(opts)
 
-        opts= nil
-        if args.size == 1 && args[0].class <= ::Hash
-          opts= args[0]
-        else
-          opts= Rails::DSL::ActionDispatchRouteEXT::Helpers.process_args(*args)
+        if opts[:class]
+          mount_controller opts
         end
 
-        # helper lambdas
+      end
 
-        create_mapping= lambda do
+      def mount_controller(opts={})
 
-          opts[:pim].each do |method_name|
+        opts[:pim].each do |method_name|
 
-            method_to_use= nil
-            [:get,:post,:put,:delete,:options].each do |pre_spec_method_call_type|
-              if opts[pre_spec_method_call_type].include?(method_name)
-                method_to_use ||= pre_spec_method_call_type
-              end
+          # #> override methods if instance method variable detected, and will passed as input from params
+          # begin
+          #   var= opts[:class].constantize.instance_method(method_name)
+          #   unless var.parameters.select{|ary|(ary[0] ==:req)}.empty?
+          #     define_method(method_name) do
+          #       var.bind(self).call(*var.parameters.select{|ary|(ary[0] == :req)}.map{|ary| ary[1] }.map{ |param_key| params[param_key] })
+          #     end
+          #   end
+          # end
+
+          #> build path by method name
+          method_to_use= nil
+          [:get,:post,:put,:delete,:options].each do |pre_spec_method_call_type|
+            if opts[pre_spec_method_call_type].include?(method_name)
+              method_to_use ||= pre_spec_method_call_type
             end
-
-            method_to_use ||= :match
-
-            url_path = opts[:urls][method_name].nil? ? "/#{method_name}" : opts[:urls][method_name].to_s
-
-            self.__send__ method_to_use,
-                          "#{url_path}#{Rails::DSL::ActionDispatchRouteEXT::Helpers.array_to_url_params(opts[:params],method_name)}",
-                          {to: "#{opts[:short_class_name]}##{method_name}", defaults: opts[:defaults].dup }
-
           end
+          method_to_use ||= :match
 
-        end
+          url_path = opts[:urls][method_name].nil? ? "/#{method_name}" : opts[:urls][method_name].to_s
 
-        # make scope
-        if !opts[:scope].nil? && opts[:scope].class <= String
-          self.scope opts[:scope] do
-            create_mapping.call
-          end
-        else
-          create_mapping.call
+          self.__send__ method_to_use,
+                        "#{url_path}#{Helpers.array_to_url_params(opts[:params],method_name)}",
+                        {to: "#{opts[:short_class_name]}##{method_name}", defaults: opts[:defaults].dup }
+
         end
 
         return nil
@@ -172,25 +169,7 @@ module Rails
 
           opts[:pim].each do |sym|
 
-            var= self.instance_method(sym)
-            parameters= []
 
-            unless var.parameters.select{|ary|(ary[0] ==:req)}.empty?
-              parameters += var.parameters.select{|ary|(ary[0] ==:req)}.map{|ary| ary[1] }
-              conv_params.push [ sym, parameters ]
-            end
-
-            define_method(sym) do
-
-              value= var.bind(self).call(*parameters.map{ |param_key| params[param_key] })
-
-              respond_to do |format|
-                format.html
-                format.json { render json:  value }
-                format.xml  { render xml:   value }
-              end
-
-            end
 
           end
 
@@ -204,19 +183,7 @@ module Rails
       end
       alias mount_rendered_controller mount_controller_with_render
 
-      def get_controller_names *path
-        path += ['app','controllers','*_controller.{ru,rb}'] if path.empty?
-        return Dir.glob(::Rails.root.join(*path)).map{|p| p.split(File::Separator).last.split('_controller')[0].to_sym }
-      end
 
-      def mount_controllers
-        get_controller_names.each { |cn| mount_controller(cn) }
-      end
-
-      def mount_controllers_with_render
-        get_controller_names.each{ |cn| mount_controller_with_render(cn) }
-      end
-      alias mount_rendered_controllers mount_controllers_with_render
 
     end
   end
