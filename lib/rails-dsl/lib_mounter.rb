edd73eca8a7747e84c5aad046314784031142d5a
Adam Luzsi <adam.luzsi@ppt-consulting.net>
2014-11-11 10:21:29 +0100

    update lib mounter

--- lib/rails-dsl/lib_mounter.rb
+++ lib/rails-dsl/lib_mounter.rb
@@ -1,11 +1,20 @@
 # in most of my project i use a boot.rb file in the libs folder for 3. party libs
 Thread.new do
 
-  sleep(0.5) while Rails.root.nil?
-  if File.exist?(Rails.root.join('lib','boot.rb'))
-    require Rails.root.join('lib','boot.rb')
-  else
-    Dir.glob( Rails.root.join('lib','*.{ru,rb}') ).each { |p| require(p) }
+  #> wait for 60 sec try else give up, no inf loop chance!
+  120.times do
+    if Rails.root.nil?
+      sleep(0.5)
+    else
+
+      if File.exist?(File.join(Rails.root,'lib','boot.rb'))
+        require File.join(Rails.root,'lib','boot.rb')
+      else
+        Dir.glob( File.join(Rails.root,'lib','*.{ru,rb}') ).each { |p| require(p) }
+      end
+      break
+
+    end
   end
 
 end
